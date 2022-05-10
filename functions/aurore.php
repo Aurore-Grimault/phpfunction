872d1b9298232f9be7cc3d9ef6a126b39f0dd035
Aurore-Grimault <auroregrimault@hotmail.fr>
2022-05-10 12:54:29 +0200

    first commit

--- functions/aurore.php
+++ functions/aurore.php
@@ -1,11 +1,39 @@
 <?php
 
-namespace firstname;
+// namespace firstname;
 
 function checkPassword($password) {
+    echo '<div class="btn-group" role="group" aria-label="Basic example">
+    <button type="button" class="btn btn-primary">Test du mot de passe</button> </div>';
+    echo '<h1 class="text-center">Aurore</h1>';
+    echo 'Mot de passe : <br>';
+    echo 'Force du mot de passe <br>';
+    echo '<div class="progress">
+    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
+  </div>';
 
-    echo '<h1 class="text-center">Firstname</h1>';
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number    = preg_match('@[0-9]@', $password);
+    $specialChars = preg_match('@[^\w]@', $password);
 
-    // ...
+if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 12) {
+    
+  echo '<div class="row">
+        <section class="card col-md-4 mx-auto my-1">    
+        <div class="form-group"> Le mot de passe doit contenir au moins : <br> </div>
+        1 chiffre <br>
+        1 minuscule <br>
+        1 majuscule <br>
+        1 caractère spécial <br>
+        12 caractères
+        </section> </div>';
+
+}else{
+    echo 'Mot de passe valide.';
+}
+    
+
 
 }
+?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    // ...
+  //
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +12,7 @@
         <title>PHP - Les fonctions</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+       
     </head>
     <body>
 
@@ -25,10 +25,15 @@
                 $password = $_GET['password'];
 
                 // I use my personal function with my namespace
-                // ...
+                         
+                include_once './functions/aurore.php';
+                checkPassword($password);
+                
 
             }
+
             else {
+
                 ?>
 
                 <div class="alert alert-info" role="alert">
