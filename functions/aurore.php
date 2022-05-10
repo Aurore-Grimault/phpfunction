6e57670a0142f92448e7b80ba3db3c8e7c7dc090
Aurore-Grimault <auroregrimault@hotmail.fr>
2022-05-10 14:51:10 +0200

    liste spécifiée

--- functions/aurore.php
+++ functions/aurore.php
@@ -7,10 +7,10 @@ function checkPassword($password) {
     <button type="button" class="btn btn-primary">Test du mot de passe</button> </div>';
     echo '<h1 class="text-center">Aurore</h1>';
     echo 'Mot de passe : <br>';
-    echo 'Force du mot de passe <br>';
+    echo '<div class="text-end">Force du mot de passe </div>';
     echo '<div class="progress">
-    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
-  </div>';
+    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div> 
+  </div> <br>';
 
     $uppercase = preg_match('@[A-Z]@', $password);
     $lowercase = preg_match('@[a-z]@', $password);
@@ -18,19 +18,24 @@ function checkPassword($password) {
     $specialChars = preg_match('@[^\w]@', $password);
 
 if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 12) {
-    
-  echo '<div class="row">
-        <section class="card col-md-4 mx-auto my-1">    
-        <div class="form-group"> Le mot de passe doit contenir au moins : <br> </div>
-        1 chiffre <br>
-        1 minuscule <br>
-        1 majuscule <br>
-        1 caractère spécial <br>
-        12 caractères
-        </section> </div>';
+   
+  echo ' <div class =row> 
+            <div class="col-4 mx-auto">   
+                <ul class="list-group">
+                    <li class="list-group-item active" aria-current="true">Le mot de passe doit contenir au moins : </li>
+                    <li class="list-group-item">1 chiffre </li>
+                    <li class="list-group-item">1 minuscule </li>
+                    <li class="list-group-item">1 majuscule </li>
+                    <li class="list-group-item">1 caractère spécial </li>
+                    <li class="list-group-item">12 caractères </li>
+                </ul>
+            </div>
+        </div>';
 
-}else{
-    echo 'Mot de passe valide.';
+} else {
+
+    echo 'Mot de passe valide. <br>
+     <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>';
 }
     
 
